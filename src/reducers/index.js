59513599dec317dff87c1525b830e16612389662
Mauricio Robayo <rfmajo@gmail.com>
2019-12-05 16:59:23 -0500

    feat: add filter reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import books from './book'
+import filter from './filter'
 
 const reducers = combineReducers({
   books,
+  filter,
 })
 
 export default reducers
