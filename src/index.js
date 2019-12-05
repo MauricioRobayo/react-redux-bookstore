d67af7f75d229a4ecd1005562337130a13688fa1
Mauricio Robayo <rfmajo@gmail.com>
2019-12-05 16:39:58 -0500

    chore: remove serviceWorker.js - again

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './reducers'
 import App from './components/App'
-import * as serviceWorker from './serviceWorker'
 import './index.css'
 
 const books = [
@@ -39,8 +38,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
