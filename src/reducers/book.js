28b1a265d407e428e7fddc014a43b266b5dea547
Mauricio Robayo <rfmajo@gmail.com>
2019-12-05 11:20:34 -0500

    feat: add sample Book reducer

---
+++ src/reducers/book.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { id: 1, title: 'JS: The Good Parts', category: 'Tech' },
+    { id: 1, title: 'Eloquent JS', category: 'Tech' },
+    { id: 1, title: "You don't know JS", category: 'Tech' },
+    { id: 1, title: 'JS Ninja', category: 'Tech' },
+  ]
+}
