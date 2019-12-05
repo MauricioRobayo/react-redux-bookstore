2160fcd2b029f8f4535b96e172b6412c6ddd51db
Mauricio Robayo <rfmajo@gmail.com>
2019-12-05 16:04:36 -0500

    feat: add handlers to book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Book = ({ book }) => (
+const Book = ({ book, handleRemoveBook }) => (
   <tr>
     <td>{book.id}</td>
     <td>{book.title}</td>
     <td>{book.category}</td>
+    <td>
+      <button type="button" onClick={() => handleRemoveBook(book)}>
+        Remove
+      </button>
+    </td>
   </tr>
 )
 
@@ -15,6 +20,7 @@ Book.propTypes = {
     title: PropTypes.string,
     category: PropTypes.string,
   }).isRequired,
+  handleRemoveBook: PropTypes.func.isRequired,
 }
 
 export default Book
