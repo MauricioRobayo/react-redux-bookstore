2a521c3d83af2f263cedb3edde4b5df52f97041e
Mauricio Robayo <rfmajo@gmail.com>
2019-12-05 11:20:15 -0500

    feat: add Book component

---
+++ src/components/Book.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Book = ({ book }) => (
+  <tr>
+    <td>{book.id}</td>
+    <td>{book.title}</td>
+    <td>{book.category}</td>
+    <td>remove</td>
+  </tr>
+)
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+    category: PropTypes.string,
+  }).isRequired,
+}
+
+export default Book
