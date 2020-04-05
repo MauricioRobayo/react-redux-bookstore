9ed6e939f20909fc561d9cf7879bad4e94d17233
Mauricio Robayo <hi@mauriciorobayo.com>
2020-04-04 21:49:11 -0500

    refactor: move initial state to componentDidMount

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,12 +1,40 @@
 import PropTypes from 'prop-types';
+import uniqid from 'uniqid';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
-import { removeBook, changeFilter } from '../actions';
+import { removeBook, createBook, changeFilter } from '../actions';
 import CategoryFilter from '../components/CategoryFilter';
 import Header from '../components/Header';
 
 class BooksList extends Component {
+  componentDidMount() {
+    const { createBook } = this.props;
+    const books = [
+      {
+        id: uniqid(),
+        title: 'JS: The Good Parts',
+        category: 'Learning',
+      },
+      {
+        id: uniqid(),
+        title: 'Eloquent JS',
+        category: 'Learning',
+      },
+      {
+        id: uniqid(),
+        title: "You don't know JS",
+        category: 'History',
+      },
+      {
+        id: uniqid(),
+        title: 'JS Ninja',
+        category: 'Biography',
+      },
+    ];
+    books.forEach(createBook);
+  }
+
   handleRemoveBook = (book) => {
     const { removeBook } = this.props;
     removeBook(book);
@@ -50,6 +78,7 @@ const mapStateToProps = ({ books, filter }) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   removeBook: (book) => dispatch(removeBook(book)),
+  createBook: (book) => dispatch(createBook(book)),
   changeFilter: (filter) => dispatch(changeFilter(filter)),
 });
 
@@ -63,6 +92,7 @@ BooksList.propTypes = {
   ).isRequired,
   filter: PropTypes.string.isRequired,
   removeBook: PropTypes.func.isRequired,
+  createBook: PropTypes.func.isRequired,
   changeFilter: PropTypes.func.isRequired,
 };
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import uniqid from 'uniqid';
-import { createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import App from './components/App';
 import './index.scss';
 
-const books = [
-  {
-    id: uniqid(),
-    title: 'JS: The Good Parts',
-    category: 'Learning',
-  },
-  {
-    id: uniqid(),
-    title: 'Eloquent JS',
-    category: 'Learning',
-  },
-  {
-    id: uniqid(),
-    title: "You don't know JS",
-    category: 'History',
-  },
-  {
-    id: uniqid(),
-    title: 'JS Ninja',
-    category: 'Biography',
-  },
-];
+const initialState = {
+  books: [],
+  filter: 'All',
+};
 
-const store = createStore(reducers, { books, filter: 'All' });
+const middleware = applyMiddleware(thunk, logger);
+
+const store = createStore(reducers, initialState, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
