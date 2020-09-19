610e682027901ad0eef35afc662f72c448042d34
MauricioRobayo <MauricioRobayo@users.noreply.github.com>
2020-09-19 20:09:38 +0000

    deploy: 8d20aa6575ed5781b698dcd55d775b0a0abfcead

---
+++ precache-manifest.ec88884362a381aabf75df0151d74230.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a48b903413e2bcf0625a33b80905431",
+    "url": "/react-redux-bookstore/index.html"
+  },
+  {
+    "revision": "bef7efa8be1e1321c488",
+    "url": "/react-redux-bookstore/static/css/main.9cef78e3.chunk.css"
+  },
+  {
+    "revision": "277b189e716f525c403d",
+    "url": "/react-redux-bookstore/static/js/2.4ce3d924.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-redux-bookstore/static/js/2.4ce3d924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef7efa8be1e1321c488",
+    "url": "/react-redux-bookstore/static/js/main.54470836.chunk.js"
+  },
+  {
+    "revision": "b77b9512ef3dc1964067",
+    "url": "/react-redux-bookstore/static/js/runtime-main.01e5faf0.js"
+  }
+]);
