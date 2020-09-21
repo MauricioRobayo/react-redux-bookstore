7a2cc98599c74339385b1bf3da6152568ea56392
Mauricio Robayo <rfmajo@gmail.com>
2020-09-21 09:54:02 -0500

    refactor: use object as arguments

--- src/api/googleBooksAPI.js
+++ src/api/googleBooksAPI.js
@@ -1,6 +1,6 @@
-const fetchBooks = async (searchParams) => {
+const fetchBooks = async ({ searchParams, endpoint = '' }) => {
   const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?${searchParams}`
+    `https://www.googleapis.com/books/v1/volumes/${endpoint}?${searchParams}`
   );
   if (!response.ok) {
     throw new Error(`Something went wrong ${response.statusText}`);
@@ -14,7 +14,7 @@ const fetchBooksByCategory = (category) => {
     maxResults: 40,
     fields: 'items(id,volumeInfo(title,imageLinks/smallThumbnail))',
   });
-  return fetchBooks(searchParams);
+  return fetchBooks({ searchParams });
 };
 
 const searchBooksByTitle = (title) => {
@@ -22,7 +22,7 @@ const searchBooksByTitle = (title) => {
     q: `intitle:${title}`,
     fields: 'totalItems,items(id,volumeInfo/title)',
   });
-  return fetchBooks(searchParams);
+  return fetchBooks({ searchParams });
 };
 
 export { fetchBooksByCategory, searchBooksByTitle };
