9c407900d8a7c230f5774a45d2f74cfdcd753790
Mauricio Robayo <rfmajo@gmail.com>
2021-03-20 08:13:58 -0500

    refactor: use implicit return on arrow function
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,23 +1,21 @@
 import PropTypes from 'prop-types';
 import React, { memo } from 'react';
 
-const Book = ({ book, handleRemoveBook }) => {
-  return (
-    <div className="Book">
-      <img src={book.thumbnail} alt="" />
-      <div className="Book-category">{book.category}</div>
-      <h2 className="Book-title">{book.title}</h2>
-      <div className="Book-id">{book.id}</div>
-      <button
-        className="Book-remove"
-        type="button"
-        onClick={() => handleRemoveBook(book)}
-      >
-        Remove
-      </button>
-    </div>
-  );
-};
+const Book = ({ book, handleRemoveBook }) => (
+  <div className="Book">
+    <img src={book.thumbnail} alt="" />
+    <div className="Book-category">{book.category}</div>
+    <h2 className="Book-title">{book.title}</h2>
+    <div className="Book-id">{book.id}</div>
+    <button
+      className="Book-remove"
+      type="button"
+      onClick={() => handleRemoveBook(book)}
+    >
+      Remove
+    </button>
+  </div>
+);
 
 Book.propTypes = {
   book: PropTypes.shape({
